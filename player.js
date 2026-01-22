ddc0c22c433a8050cd80f690de34059bd7e3059e
jonathan <112445400+submachinegunnn@users.noreply.github.com>
2026-01-21 20:17:13 -0600

    Update player.js

--- player.js
+++ player.js
@@ -1,23 +1,11 @@
-const p = new URLSearchParams(location.search);
-const type = p.get("type");
-const id = p.get("id");
-const s = p.get("s") || 1;
-const e = p.get("e") || 1;
+const params = new URLSearchParams(location.search);
+const id = params.get("id");
+const type = params.get("type");
 
-const frame = document.getElementById("frame");
-let url;
+const iframe = document.getElementById("player");
 
-if (type === "movie") {
-  url = `https://www.vidking.net/embed/movie/${id}?autoPlay=true`;
+if (type === "tv") {
+  iframe.src = `https://www.vidking.net/embed/tv/${id}/1/1?autoPlay=true&nextEpisode=true`;
 } else {
-  url = `https://www.vidking.net/embed/tv/${id}/${s}/${e}?autoPlay=true&nextEpisode=true`;
-}
-
-frame.src = url;
-
-/* Save continue watching */
-const list = JSON.parse(localStorage.getItem("continue") || "[]");
-if (!list.find(i => i.id == id)) {
-  list.unshift({ id, media_type: type });
-  localStorage.setItem("continue", JSON.stringify(list.slice(0, 10)));
+  iframe.src = `https://www.vidking.net/embed/movie/${id}?autoPlay=true`;
 }
