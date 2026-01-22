aaedf8a7e27c525fb23952a5fac9d345cdee5f5b
jonathan <112445400+submachinegunnn@users.noreply.github.com>
2026-01-21 18:12:18 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
+const moviesEl = document.getElementById('movies');
+
 async function loadMovies() {
   const res = await fetch('/api/tmdb?path=/movie/popular');
   const data = await res.json();
 
-  movies.innerHTML = data.results.map(m => `
-    <div>
-      <img src="https://image.tmdb.org/t/p/w300${m.poster_path}">
-      <h3>${m.title}</h3>
+  moviesEl.innerHTML = data.results.map(movie => `
+    <div class="movie" onclick="openMovie(${movie.id})">
+      <img src="https://image.tmdb.org/t/p/w300${movie.poster_path}">
+      <h3>${movie.title}</h3>
     </div>
   `).join('');
+}
+
+function openMovie(id) {
+  window.location.href = `/watch.html?id=${id}`;
 }
 
 loadMovies();
