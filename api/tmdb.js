0673b3054c963ecf83cfaf6ebbb3ccba832d57a6
jonathan <112445400+submachinegunnn@users.noreply.github.com>
2026-01-21 18:09:50 -0600

    Refactor TMDB API handler to simplify request

--- api/tmdb.js
+++ api/tmdb.js
@@ -1,13 +1,6 @@
 export default async function handler(req, res) {
-const { path } = req.query;
-if (!path) return res.status(400).end();
-
-
-const url = `https://api.themoviedb.org/3/${path}?api_key=${process.env.TMDB_KEY}`;
-const r = await fetch(url);
-const data = await r.json();
-
-
-res.setHeader('Cache-Control', 's-maxage=600, stale-while-revalidate');
-res.status(200).json(data);
+  const url = `https://api.themoviedb.org/3${req.query.path}?api_key=${process.env.TMDB_KEY}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  res.status(200).json(data);
 }
