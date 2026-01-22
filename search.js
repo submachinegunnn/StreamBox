9ba60660062e925210e18d0b56e733cd5fb3467d
jonathan <112445400+submachinegunnn@users.noreply.github.com>
2026-01-21 20:12:17 -0600

    Update search.js

--- search.js
+++ search.js
@@ -1,1 +1,25 @@
+import { tmdb, IMG } from "./api/tmdb.js";
 
+const q = new URLSearchParams(location.search).get("q");
+const grid = document.getElementById("grid");
+
+function card(item) {
+  const el = document.createElement("div");
+  el.className = "card";
+  el.innerHTML = `<img src="${IMG}${item.poster_path}">`;
+
+  el.onclick = () => {
+    if (item.media_type === "tv") {
+      location.href = `episode.html?id=${item.id}`;
+    } else {
+      location.href = `player.html?type=movie&id=${item.id}`;
+    }
+  };
+
+  return el;
+}
+
+(async () => {
+  const data = await tmdb(`/search/multi?query=${q}`);
+  data.results.filter(i => i.poster_path).forEach(i => grid.append(card(i)));
+})();
