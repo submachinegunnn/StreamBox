9380f8a021eae1546bff3ca7c90b19f22c5cf597
jonathan <112445400+submachinegunnn@users.noreply.github.com>
2026-01-21 18:12:48 -0600

    Update tmdb.js

--- api/tmdb.js
+++ api/tmdb.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const url = `https://api.themoviedb.org/3${req.query.path}?api_key=${process.env.TMDB_KEY}`;
-  const response = await fetch(url);
-  const data = await response.json();
+  const r = await fetch(url);
+  const data = await r.json();
   res.status(200).json(data);
 }
