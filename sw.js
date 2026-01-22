44652bb6944c0e799b1560f95433b7809dae762e
jonathan <112445400+submachinegunnn@users.noreply.github.com>
2026-01-21 19:29:04 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,1 @@
-self.addEventListener('install',e=>{
-e.waitUntil(caches.open('streambox').then(c=>c.addAll(['/','/styles.css','/app.js'])));
-});
-self.addEventListener('fetch',e=>{
-e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
+self.addEventListener("fetch", () => {});
