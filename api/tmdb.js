e2ce9404a595613490b98ba07d274f5406656a2f
jonathan <112445400+submachinegunnn@users.noreply.github.com>
2026-01-21 19:36:18 -0600

    Update tmdb.js

--- api/tmdb.js
+++ api/tmdb.js
@@ -1,10 +1,6 @@
 export default async function handler(req, res) {
   const endpoint = req.query.endpoint;
 
-  if (!endpoint) {
-    return res.status(400).json({ error: "Missing endpoint" });
-  }
-
   const response = await fetch(
     `https://api.themoviedb.org/3${endpoint}`,
     {
